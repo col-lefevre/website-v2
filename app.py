b509c693a3c99be97406cb243db47e80fa68a008
clefevr <clefevr@iu.edu>
2023-08-13 15:31:44 -0400

    navbar

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+nav_links = {"index": "About", "research": "Research", "cv": "CV", }
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_name="index")
