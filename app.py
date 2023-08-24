78e8c2d494bf317afb56b178b188a3657d1b62d3
Colin LeFevre <colin.lefevre.school@gmail.com>
2023-08-24 15:58:27 -0400

    updated nav and pages

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-nav_links = {"index": "About", "research": "Research", "cv": "CV", }
-
 @app.route('/')
 def index():
     return render_template("index.html", page_name="index")
+
+@app.route('/research')
+def research():
+    return render_template("research.html", page_name="research")
+
+@app.route('/design')
+def design():
+    return render_template("design.html", page_name="design")
